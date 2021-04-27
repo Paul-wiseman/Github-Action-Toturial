185850956533430e78e3bdce017f348a5c0ae22a
Paul-wiseman <paulndibe92@gmail.com>
2021-04-27 19:43:55 +0100

    first commit

---
+++ app/src/main/java/com/wiseman/paul/githubactiontutorial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wiseman.paul.githubactiontutorial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
